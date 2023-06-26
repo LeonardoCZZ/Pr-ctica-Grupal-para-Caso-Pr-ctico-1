bf6fe46f3ead9ae3d7b05a1e4b8cac323ade7f40
Mayerling95 <87285303+Mayerling95@users.noreply.github.com>
2023-06-26 16:22:43 -0600

    Update FirebaseStorageService.java

--- src/main/java/com/practica01/service/FirebaseStorageService.java
+++ src/main/java/com/practica01/service/FirebaseStorageService.java
@@ -2,12 +2,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.tienda.Service;
+package com.tienda.service;
 
-/**
- *
- * @author Mayerling
- */
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.Multipart
+
+File;
+
+@Service
 public interface FirebaseStorageService {
-    
-}
+
+    public String cargaImagen (MultipartFile archivoLocalCliente, String carpeta, Long id);
+    //E1 BuketName es el <id_del_proyecto> + .appspot.com/
+    final String BucketName = "practica1-96a3e.appspot.com";
+    //Esta es la ruta básica de este proyecto 
+    final String rutaSuperiorStorage = "practica1";
+    //Ubicación donde se encuentra el archivo de configuración Json
+    final String rutaJsonFile "firebase";
+    //El nombre del archivo Json
+    final String archivoJsonFile = "path/to/serviceAccountKey.json";

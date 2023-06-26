1e18e84c9cee080deee392928a93a096f2c28c44
Mayerling95 <87285303+Mayerling95@users.noreply.github.com>
2023-06-26 16:35:08 -0600

    Update FirebaseStorageService.java

--- src/main/java/com/practica01/FirebaseStorageService.java
+++ src/main/java/com/practica01/FirebaseStorageService.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package com.tienda.service;
+package com.practica01.service;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.Multipart
